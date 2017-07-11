137fe683a558f092d23dfd80df6ee2d46e909b31
cmkaul <cmkaul@gmail.com>
2017-07-11 10:37:25 -0700

    updating options

--- generate_paramlist.py
+++ generate_paramlist.py
@@ -77,6 +77,10 @@ def Soares():
     paramlist['turbulence']['EDMF_PrognosticTKE']['tke_diss_coeff'] = 0.01
     paramlist['turbulence']['EDMF_PrognosticTKE']['max_area_factor'] = 1.0
     paramlist['turbulence']['EDMF_PrognosticTKE']['vel_pressure_coeff'] = 1.0
+    paramlist['turbulence']['EDMF_PrognosticTKE']['entrainment_factor'] = 1.0
+    paramlist['turbulence']['EDMF_PrognosticTKE']['detrainment_factor'] = 1.0
+
+
 
     paramlist['turbulence']['EDMF_BulkSteady'] = {}
     paramlist['turbulence']['EDMF_BulkSteady']['surface_area'] = 0.1
