c5c2e04603fed750be3605b25b0eba7c3b273d05
Colleen Kaul <cmkaul@gmail.com>
2018-03-15 20:30:07 +0000

    surface_functions.pyx edited online with Bitbucket

--- surface_functions.pyx
+++ surface_functions.pyx
@@ -2,6 +2,8 @@ import numpy as np
 from thermodynamic_functions cimport latent_heat, pd_c, pv_c, sd_c, sv_c, cpm_c
 include "parameters.pxi"
 
+#Adapated from PyCLES: https://github.com/pressel/pycles
+
 cdef double buoyancy_flux(double shf, double lhf, double T_b, double qt_b, double alpha0_0):
     cdef:
         double cp_ = cpm_c(qt_b)
